a56a88e933fe206d86f7ecfa0cf4cc00c2a59037
walterlife <347783197@qq.com>
2015-07-17 17:25:54 +0800

    -madd HibernateSample

--- java-ssh/HibernateSample/src/main/java/com/walterlife/blog/App.java
+++ java-ssh/HibernateSample/src/main/java/com/walterlife/blog/App.java
@@ -1,13 +1,26 @@
 package com.walterlife.blog;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.walterlife.persistence.HibernateUtil;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Maven + Hibernate + MySQL");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+ 
+        Transaction tx = session.beginTransaction();
+        Stock stock = new Stock();
+ 
+        stock.setStockCode("33");
+        stock.setStockName("walter");
+ 
+        session.save(stock);
+        tx.commit();
     }
 }
